984b3e3fa38e90aa046a5f51d08ca4d849330d0a
Lucyhigh <yeeunsong268@gmail.com>
2022-05-27 11:43:32 +0900

    아바타 적용 및 스크립트 작성시작

--- Assets/Scripts/Player/CPlayerCamera.cs
+++ Assets/Scripts/Player/CPlayerCamera.cs
@@ -7,18 +7,21 @@ public class CPlayerCamera : MonoBehaviour
     #region 변수
     public Transform target;
 
-    public float distance = 2.0f;
+    public float distance = 2.5f;
     public float height = 1.0f;
 
     public float X_RotateSpeed = 100.0f;
     public float Y_RotateSpeed = 100.0f;
 
+    public float X_MinLimit = 60.0f;
+    public float X_MaxLimit = 60.0f;
+
     public float Y_MinLimit = -20.0f;
-    public float Y_MaxLimit = 80.0f;
+    public float Y_MaxLimit = 45.0f;
     #endregion
 
-    private float x = 0.0f;
-    private float y = 0.0f;
+    private float x = 0.0f;     //카메라 각도 x
+    private float y = 0.0f;     //카메라 각도 y
     
     float ClampAngle(float angle, float min, float max)
     {
@@ -28,13 +31,38 @@ public class CPlayerCamera : MonoBehaviour
         return Mathf.Clamp(angle, min, max);
     }
 
-    void Start()
+    Transform oTransform;
+
+    void Awake()
     {
         //Cursor.lockState = CursorLockMode.Locked;
         Vector3 angles = transform.eulerAngles;
 
         x = angles.x;
         y = angles.y;
+    }
+
+    void Update()
+    {
+        Balance();
+
+        if (chanController.isGrounded)
+        {
+            GroundChecking();
+            MoveUnityChan(1.0f);
+        }
+        else
+        {
+            // 관성에 영향을 준다.
+            move.y -= gravity * Time.deltaTime;
+            MoveUnityChan(0.01f);
+        }
+        chanController.Move(move * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            chanAnimator.SetTrigger("aJump");
+        }
     }
 
     void LateUpdate()
@@ -49,6 +77,7 @@ public class CPlayerCamera : MonoBehaviour
             x += Input.GetAxis("Mouse X") * X_RotateSpeed * 0.015f;
             y -= Input.GetAxis("Mouse Y") * Y_RotateSpeed * 0.015f;
 
+            x = ClampAngle(x, X_MinLimit, X_MaxLimit);
             y = ClampAngle(y, Y_MinLimit, Y_MaxLimit);
 
             Quaternion rotation = Quaternion.Euler(y, x, 0);
